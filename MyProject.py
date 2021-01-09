ab9d82cc66c76060768befccd1ecd59a433bf7b7
Sonny Ornido <sonnyornido0@gmail.com>
2021-01-09 10:49:17 +0800

    January 9 2021

--- MyProject.py
+++ MyProject.py
@@ -57,6 +57,9 @@ class realMainWindow(QMainWindow):
             self.ui.label_ticketValue1.setText(("CU0") + str(value1))
             self.ui.label_ticketValue.setText(("CU0") + str(value1))
         self.ui.label_roomNo.setText("Room 101")
+        self.ui.frame_rightBody.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:0.688, "
+                                              "y2:0.306818, stop:0 rgba(204, 196, 183, 255), stop:1 rgba(211, 227, "
+                                              "255, 255));")
         value1 += 1
     def ticketIncrement2(self):
         global value2
@@ -70,6 +73,9 @@ class realMainWindow(QMainWindow):
             self.ui.label_ticketValue2.setText(("VC0") + str(value2))
             self.ui.label_ticketValue.setText(("VC0") + str(value2))
         self.ui.label_roomNo.setText("Room 102")
+        self.ui.frame_rightBody.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:0.994318, "
+                                              "y2:0.023, stop:0 rgba(255, 194, 237, 255), stop:1 rgba(175, 255, 231, "
+                                              "255));")
         value2 += 1
     def ticketIncrement3(self):
         global value3
@@ -83,6 +89,8 @@ class realMainWindow(QMainWindow):
             self.ui.label_ticketValue3.setText(("DT0") + str(value3))
             self.ui.label_ticketValue.setText(("DT0") + str(value3))
         self.ui.label_roomNo.setText("Room 103")
+        self.ui.frame_rightBody.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:1, y2:0, "
+                                              "stop:0 rgba(245, 184, 168, 255), stop:1 rgba(164, 178, 255, 255));")
         value3 += 1
 if __name__ == "__main__":
     app = QApplication(sys.argv)
