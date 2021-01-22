8c191685ae30248210a637a51c47cfe944ef5b8b
Sonny Ornido <sonnyornido0@gmail.com>
2021-01-22 12:35:49 +0800

    fixing error

--- createDB.py
+++ createDB.py
@@ -3,20 +3,19 @@ import sqlite3
 conn = sqlite3.connect("patients")
 
 c = conn.cursor()
-"""
-c.execute(''' CREATE TABLE checkUp(
+
+c.execute(''' CREATE TABLE dental(
             ticket_id       INTEGER     PRIMARY KEY,
             full_name       TEXT(50)    NOT NULL,
             age             INTEGER     NOT NULL,
             address         TEXT(100)   NOT NULL,
             contact         INTEGER,
-            time_in         TEXT,
-            time_out        TEXT
-            )''');
-"""
-#c.execute("INSERT INTO checkUp (full_name, age, address, contact, time_in, time_out) "
-          #"VALUES ('Miko Reyes', 35, 'Punturin', 4523523, '', '')")
-c.execute("SELECT * FROM checkUp")
+            time_in         TEXT
+            )''')
+
+c.execute("INSERT INTO dental (full_name, age, address, contact, time_in) "
+          "VALUES ('Miko Reyes', 35, 'Punturin', 4523523, '')")
+c.execute("SELECT * FROM dental")
 print(c.fetchall())
 conn.commit()
 conn.close()
