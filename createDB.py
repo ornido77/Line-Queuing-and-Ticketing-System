45fff34499fab9986556c6dceb42c5dc4b561fc5
Sonny Ornido <sonnyornido0@gmail.com>
2021-01-22 13:31:22 +0800

    working date and time

--- createDB.py
+++ createDB.py
@@ -4,17 +4,18 @@ conn = sqlite3.connect("patients")
 
 c = conn.cursor()
 
-c.execute(''' CREATE TABLE checkUp(
+c.execute(''' CREATE TABLE dental(
             ticket_id       INTEGER     PRIMARY KEY,
             full_name       TEXT(50)    NOT NULL,
             age             INTEGER     NOT NULL,
             address         TEXT(100)   NOT NULL,
             contact         INTEGER,
-            time_in         DATETIME DEFAULT (datetime('now','localtime')
+            time_in         DATETIME DEFAULT (datetime('now','localtime'))
             )''')
-c.execute("INSERT INTO CheckUp (full_name, age, address, contact) "
-          "VALUES ('Miko Reyes', 35, 'Punturin', 4523523)")
-c.execute("SELECT * FROM CheckUp")
+
+c.execute("INSERT INTO dental (full_name, age, address, contact) "
+          "VALUES ('Niko Reyes', 35, 'Punturin', 4523523)")
+c.execute("SELECT * FROM dental")
 print(c.fetchall())
 conn.commit()
 conn.close()
